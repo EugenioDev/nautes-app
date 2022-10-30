629108474a2395c256e027d235de531225055849
Eugenio Marino <eugenio.marino@link-up.it>
2022-10-30 10:14:27 +0100

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import PirateDrunk from "./components/PirateDrunk";
-import UserList from "./components/UserList";
-import TodoForm from "./components/TodoForm";
 import Navbar from "./components/Navbar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./Pages/Home";
@@ -21,12 +18,7 @@ function App() {
          <Route path='/customHook' element={<CustomHook/>}/>
          <Route path='/todoapp' element={<TodoApp/>}/>
        </Routes>
-
      </BrowserRouter>
-
-   {/*//   <PirateDrunk/>*/}
-   {/*//   <UserList/>*/}
-   {/*//   <TodoForm/>*/}
     </div>
   );
 }
