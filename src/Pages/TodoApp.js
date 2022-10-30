fb9ebee2fefe03b7b2f87d4059254a450aea5f51
Eugenio Marino <eugenio.marino@link-up.it>
2022-10-30 11:28:21 +0100

    Fixed little UI thinks

--- src/Pages/TodoApp.js
+++ src/Pages/TodoApp.js
@@ -2,7 +2,7 @@ import TodoForm from "../components/TodoForm";
 
 const TodoApp = () => {
   return (
-   <div className='border h-full p-5 flex items-center justify-center'>
+   <div className='h-full p-5 flex items-center justify-center'>
      <TodoForm/>
    </div>
   )
